30c9d77aadafc2b102cae63d82233f63213cce57
shadymeowy <tolgademirdal@gmail.com>
2025-06-07 23:56:49 +0300

    add run_podman.sh for using podman instead of docker

---
+++ docker/run_podman.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+xhost +local:root
+mkdir -p $HOME/.ws/vinsfusion
+podman run \
+    -it \
+    --rm \
+    --net=host \
+    --privileged \
+    --device nvidia.com/gpu=all \
+    --security-opt=label=disable \
+    --env="DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --device=/dev/dri \
+    --device=/dev/video0 \
+    -v $HOME/.ws/vinsfusion/:/root/catkin_ws/ \
+    -v $(pwd)/:/root/catkin_ws/src/VINS-Fusion/ \
+    -v /run/media/tdemirdal/tb/:/datasets/ \
+    ros:vins-fusion \
+    /bin/bash -c \
+    "cd /root/catkin_ws/; source devel/setup.bash; bash"
