33ce42df9aa6bd6c3cc594ded26188c2cf25e8a4
shadymeowy <tolgademirdal@gmail.com>
2025-06-08 00:27:28 +0300

    mark many members and methods of FeatureTracker as private

--- vins_estimator/src/featureTracker/feature_tracker.h
+++ vins_estimator/src/featureTracker/feature_tracker.h
@@ -40,27 +40,26 @@ class FeatureTracker {
   FeatureTracker();
   map<int, vector<pair<int, Eigen::Matrix<double, 7, 1>>>> trackImage(
       double _cur_time, const cv::Mat &_img, const cv::Mat &_img1 = cv::Mat());
-  void setMask();
   void readIntrinsicParameter(const vector<string> &calib_file);
+  void setPrediction(map<int, Eigen::Vector3d> &predictPts);
+  void removeOutliers(set<int> &removePtsIds);
+  cv::Mat getTrackImage();
+
+ private:
+  void setMask();
   void showUndistortion(const string &name);
   void rejectWithF();
-  void undistortedPoints();
   vector<cv::Point2f> undistortedPts(vector<cv::Point2f> &pts,
                                      camodocal::CameraPtr cam);
   vector<cv::Point2f> ptsVelocity(vector<int> &ids, vector<cv::Point2f> &pts,
                                   map<int, cv::Point2f> &cur_id_pts,
                                   map<int, cv::Point2f> &prev_id_pts);
-  void showTwoImage(const cv::Mat &img1, const cv::Mat &img2,
-                    vector<cv::Point2f> pts1, vector<cv::Point2f> pts2);
   void drawTrack(const cv::Mat &imLeft, const cv::Mat &imRight,
                  vector<int> &curLeftIds, vector<cv::Point2f> &curLeftPts,
                  vector<cv::Point2f> &curRightPts,
                  map<int, cv::Point2f> &prevLeftPtsMap);
-  void setPrediction(map<int, Eigen::Vector3d> &predictPts);
-  double distance(cv::Point2f &pt1, cv::Point2f &pt2);
-  void removeOutliers(set<int> &removePtsIds);
-  cv::Mat getTrackImage();
   bool inBorder(const cv::Point2f &pt);
+  double distance(cv::Point2f &pt1, cv::Point2f &pt2);
 
   int row, col;
   cv::Mat imTrack;
